cf8cc4202da0891e1115103f71db20fc5f6490a1
cristijora <joracristi@gmail.com>
2017-06-13 22:07:27 +0300

    Auto install tabs plugin if Vue is defined in window

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-module.exports = {
-
+var VueTabsPlugin = {
   VueTabs: require('./components/VueTabs.vue'),
   VTab: require('./components/TabContent.vue'),
 
@@ -7,4 +6,10 @@ module.exports = {
     Vue.component('vue-tabs', module.exports.VueTabs)
     Vue.component('v-tab', module.exports.VTab)
   }
+}
+module.exports = VueTabsPlugin
+
+// Automatic installation if Vue has been added to the global scope.
+if (typeof window !== 'undefined' && window.Vue) {
+  window.Vue.use(VueTabsPlugin)
 }
