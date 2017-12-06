943fc7c5e5ef44b7f824a5d41ee4d19f6b65d579
André Hoendgen <hello@andre-hoendgen.de>
2017-12-06 09:04:39 +0100

    revert changing build files

--- dist/vue-tabs.common.js
+++ dist/vue-tabs.common.js
@@ -195,12 +195,6 @@ var VueTabs = {
                     [_this.textPosition === 'top' && _this.renderTabTitle(index, _this.textPosition), h(
                         'a',
                         {
-                            on: {
-                              'click': function click(event) {
-                                event.preventDefault();
-                                return false;
-                              }
-                            },
                             attrs: { href: '#',
 
                                 'aria-selected': active,
